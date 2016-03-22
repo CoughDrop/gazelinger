d6ac2df8fbd7321172ee4011d8837830a380d8a1
Brian Whitmer <brian.whitmer@gmail.com>
2016-03-22 17:03:35 -0600

    pick first valid ping result

--- gazelinger.js
+++ gazelinger.js
@@ -31,8 +31,7 @@
   }
 
   var poll = null;
-  var last_linger_ts = null;
-  var last_gaze_ts = null;
+  var lasts = {};
   var gaze_history = [];
   var callbacks = [];
   var run_callbacks = function(message) {
@@ -48,22 +47,37 @@
       if(!poll) {
         poll = setInterval(function() {
           if(callbacks.length == 0) { return; }
-          var data = null;
+          var data = {};
           if(eyex && eyex.ping) {
-            data = eyex.ping()
-          } else if(eyetribe && eyetribe.ping) {
-            data = eyetribe.ping();
-          } else if(mygaze && mygaze.ping) {
+            data.eyex = eyex.ping();
+            if(data.eyex && (data.eyex.gaze_ts == 0 || lasts.eyex == data.eyex.gaze_ts)) {
+              data.eyex = null;
+            } else if(data.eyex) {
+              lasts.eyex = data.eyex.gaze_ts;
+            }
+          }
+          if(eyetribe && eyetribe.ping) {
+            data.eyetribe = eyetribe.ping();
+            if(data.eyetribe && (data.eyetribe.gaze_ts == 0 || lasts.eyetribe == data.eyetribe.gaze_ts)) {
+              data.eyetribe = null;
+            } else if(data.eyetribe){
+              lasts.eyetribe = data.eyetribe.gaze_ts
+            }
+          }
+          if(mygaze && mygaze.ping) {
             // TODO ...
           }
+          data.result = data.eyex || data.eyetribe || data.mygaze;
+          data = data.result;
+          
 
           var message = { raw: data };
           message.ts = (new Date()).getTime();
 
-          if(data.end_ts && data.begin_ts && data.end_ts > data.begin_ts && data.end_ts != last_linger_ts) {
+          if(data.end_ts && data.begin_ts && data.end_ts > data.begin_ts && data.end_ts != lasts.linger) {
 //             console.log("linger duration " + (data.end_ts - data.begin_ts));
-//             console.log("time since last " + (data.end_ts - last_linger_ts));
-            last_linger_ts = data.end_ts;
+//             console.log("time since last " + (data.end_ts - lasts.linger));
+            lasts.linger = data.end_ts;
 
             message = { raw: data};
             message.ts = (new Date()).getTime();
@@ -73,8 +87,8 @@
             message.type = 'linger';
             run_callbacks(message);
           }
-          if(data.gaze_ts && data.gaze_ts != last_gaze_ts) {
-            last_gaze_ts = data.gaze_ts;
+          if(data.gaze_ts && data.gaze_ts != lasts.gaze) {
+            lasts.gaze = data.gaze_ts;
             message = { raw: data};
             message.ts = (new Date()).getTime();
             message.screenX = data.gaze_x; //(data.gaze_x / ratio) - (window.screenInnerOffsetX || window.screenX);
@@ -91,7 +105,7 @@
           
             // prune based on distance from latest timestamp
             var new_history = [];
-            gaze_history.forEach(function (e) { if (last_gaze_ts - e.ts < 200) { new_history.push(e); } });
+            gaze_history.forEach(function (e) { if (lasts.gaze - e.ts < 200) { new_history.push(e); } });
             gaze_history = new_history;
           
             // find a quick median
@@ -108,8 +122,8 @@
               var filtered_history = gaze_history.filter(function (e) { return (Math.abs(e.x - midx) < 50) && (Math.abs(e.y - midy) < 50); });
               if (filtered_history.length > 0) {
                 var biggest_dist = 0;
-                last_history_ts = gaze_history[0].ts;
-                filtered_history.forEach(function (e) { biggest_dist = Math.max(biggest_dist, e.ts - last_history_ts); last_history_ts - e.ts; });
+                lasts.history = gaze_history[0].ts;
+                filtered_history.forEach(function (e) { biggest_dist = Math.max(biggest_dist, e.ts - lasts.history); lasts.history - e.ts; });
               
                 // if there are no significant time gaps, compute a new middle and trigger a linger event
                 if (biggest_dist <= 50) {
