c9b9a0c1e790233fa383c9e6e62f4034f38280b2
Brian Whitmer <brian.whitmer@gmail.com>
2017-02-17 17:42:21 -0700

    fix averaged check

--- gazelinger.js
+++ gazelinger.js
@@ -95,60 +95,64 @@
             message.type = 'linger';
 //            run_callbacks(message);
           }
-          if(data.gaze_ts && data.gaze_ts != lasts.gaze && any_averaged) {
+          if(data.gaze_ts && data.gaze_ts != lasts.gaze) {
             lasts.gaze = data.gaze_ts;
             message = { raw: data};
             message.ts = (new Date()).getTime();
             message.screenX = data.gaze_x; //(data.gaze_x / ratio) - (window.screenInnerOffsetX || window.screenX);
             message.screenY = data.gaze_y; //(data.gaze_y / ratio) - (window.screenInnerOffsetY || window.screenY);
+            message.duration = 50;
             message.type = 'over';
             message.ts = data.gaze_ts;
             run_callbacks(message);
 
-            gaze_history.push({
-              x: message.screenX,
-              y: message.screenY,
-              ts: message.ts
-            });
+            if(any_averaged) {
+              gaze_history.push({
+                x: message.screenX,
+                y: message.screenY,
+                ts: message.ts
+              });
           
-            // prune based on distance from latest timestamp
-            var new_history = [];
-            gaze_history.forEach(function (e) { if (lasts.gaze - e.ts < 200) { new_history.push(e); } });
-            gaze_history = new_history;
+              // prune based on distance from latest timestamp
+              var new_history = [];
+              gaze_history.forEach(function (e) { if (lasts.gaze - e.ts < 200) { new_history.push(e); } });
+              gaze_history = new_history;
           
-            // find a quick median
-            var xs = gaze_history.sort(function (a, b) { return b.x - a.x });
-            var midx = xs[Math.ceil(xs.length / 2)];
-            var ys = gaze_history.sort(function (a, b) { return b.y - a.y });
-            var midy = ys[Math.ceil(ys.length / 2)];
+              // find a quick median
+              var xs = gaze_history.sort(function (a, b) { return b.x - a.x });
+              var midx = xs[Math.ceil(xs.length / 2)];
+              var ys = gaze_history.sort(function (a, b) { return b.y - a.y });
+              var midy = ys[Math.ceil(ys.length / 2)];
           
-            // temporarily remove outliers
-            if (midx && midy) {
-              midx = midx.x;
-              midy = midy.y;
+              // temporarily remove outliers
+              if (midx && midy) {
+                midx = midx.x;
+                midy = midy.y;
             
-              var filtered_history = gaze_history.filter(function (e) { return (Math.abs(e.x - midx) < 50) && (Math.abs(e.y - midy) < 50); });
-              if (filtered_history.length > 0) {
-                var biggest_dist = 0;
-                lasts.history = gaze_history[0].ts;
-                filtered_history.forEach(function (e) { biggest_dist = Math.max(biggest_dist, e.ts - lasts.history); lasts.history - e.ts; });
+                var filtered_history = gaze_history.filter(function (e) { return (Math.abs(e.x - midx) < 50) && (Math.abs(e.y - midy) < 50); });
+                if (filtered_history.length > 0) {
+                  var biggest_dist = 0;
+                  lasts.history = gaze_history[0].ts;
+                  filtered_history.forEach(function (e) { biggest_dist = Math.max(biggest_dist, e.ts - lasts.history); lasts.history - e.ts; });
               
-                // if there are no significant time gaps, compute a new middle and trigger a linger event
-                if (biggest_dist <= 50) {
-                  var mean_x = 0;
-                  var mean_y = 0;
-                  filtered_history.forEach(function (e) { mean_x = mean_x + e.x; mean_y = mean_y + e.y; });
-                  mean_x = mean_x / filtered_history.length;
-                  mean_y = mean_y / filtered_history.length;
+                  // if there are no significant time gaps, compute a new middle and trigger a linger event
+                  if (biggest_dist <= 50) {
+                    var mean_x = 0;
+                    var mean_y = 0;
+                    filtered_history.forEach(function (e) { mean_x = mean_x + e.x; mean_y = mean_y + e.y; });
+                    mean_x = mean_x / filtered_history.length;
+                    mean_y = mean_y / filtered_history.length;
                 
-                  message = { raw: data};
-                  message.ts = (new Date()).getTime();
-                  message.screenX = mean_x;
-                  message.screenY = mean_y;
-                  message.type = 'linger';
-                  message.duration = filtered_history[filtered_history.length - 1].ts - filtered_history[0].ts;
-                  run_callbacks(message);
-                  gaze_history = gaze_history.slice(4, 50);
+                    message = { raw: data};
+                    message.ts = (new Date()).getTime();
+                    message.screenX = mean_x;
+                    message.screenY = mean_y;
+                    message.type = 'linger';
+                    message.duration = filtered_history[filtered_history.length - 1].ts - filtered_history[0].ts;
+                    run_callbacks(message);
+                    // slicing at index 3 means you will get at most a linger event every 150ms
+                    gaze_history = gaze_history.slice(3, 50);
+                  }
                 }
               }
             }
