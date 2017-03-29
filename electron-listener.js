87747558a759d5a8aa232930896ef48916b2e505
Brian Whitmer <brian.whitmer@gmail.com>
2017-03-29 12:52:41 -0600

    send hardware information through

--- electron-listener.js
+++ electron-listener.js
@@ -38,6 +38,7 @@
         e.clientX = data.x;
         e.clientY = data.y;
         e.duration = data.duration;
+        e.eyegaze_hardware = data.hardware;
         e.ts = data.ts;
 
         var elem_left = elem && elem.style && elem.style.left;
